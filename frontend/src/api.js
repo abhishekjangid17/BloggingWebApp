77c8e9acd9b9812cdff68137a1f5252b72b08ad0
abhishekjangid1 <abjangid222@gmail.com>
2026-05-27 09:51:32 +0530

    fix: switch api back to production url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,23 @@
-import axios from 'axios';
+// import axios from 'axios';
 
+// const API = axios.create({
+//   baseURL: 'https://bloggingwebapp-d2o3.onrender.com', // ✅ local backend
+//   withCredentials: true,
+// });
+
+// export default API;
+
+// import axios from 'axios';
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000', // ✅ local backend
+//   withCredentials: true,
+// });
+// export default API;
+
+
+import axios from 'axios';
 const API = axios.create({
-  baseURL: 'https://bloggingwebapp-d2o3.onrender.com', // ✅ local backend
+  baseURL: 'https://bloggingwebapp-d2o3.onrender.com',
   withCredentials: true,
 });
-
 export default API;
